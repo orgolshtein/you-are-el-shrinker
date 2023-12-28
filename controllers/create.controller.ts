43b58b3d670fb5dc096481e284c73c0f33486699
Eli-N-Or 2 <eli-n-or@outlook.com>
2023-12-28 10:40:34 +0200

    random unique has function added to create router

--- controllers/create.controller.ts
+++ controllers/create.controller.ts
@@ -4,15 +4,25 @@ import * as links_model from "../models/links.model.js";
 import { LinkObject, RedirectObject, port, host, prod_link } from "../index.js";
 import { asyncHandler } from "../middleware/async.handler.js";
 
+const createUniqueHash = asyncHandler(async (): Promise<string | void> => {
+  const randomHash: string = (Math.random() + 1).toString(36).substring(5);
+  const linkCompare: LinkObject | undefined = await links_model.getLink(randomHash, null);
+  if (linkCompare === undefined){
+    return randomHash
+  } else {
+    createUniqueHash();
+  }
+});
+
 export const createLink = asyncHandler(async (
   target_value: string, 
   param: string
   ): Promise<RedirectObject | undefined> => {
-  const randomHash: string = (Math.random() + 1).toString(36).substring(5);
+  const randomUniqueHash: string = await createUniqueHash();
   const linkObj: LinkObject | undefined = await links_model.getLink(target_value, "by_target", param);
   const newRedirectObj: RedirectObject = {
     _id: new ObjectId,
-    link: randomHash,
+    link: randomUniqueHash,
     visits: 0,
     last_visit: "None",
     last_visit_ms: 0
@@ -24,10 +34,10 @@ export const createLink = asyncHandler(async (
       shrinks: [{...newRedirectObj}]
     }
     await links_model.createLink(newLinkObj);
-    return {...newRedirectObj, output: `${prod_link}/${randomHash}`}
+    return {...newRedirectObj, output: `${prod_link}/${randomUniqueHash}`}
   } else {
     linkObj.shrinks.push(newRedirectObj);
     await links_model.updateLink(linkObj);
-    return {...newRedirectObj, output: `${prod_link}/${randomHash}`}
+    return {...newRedirectObj, output: `${prod_link}/${randomUniqueHash}`}
   }
 });
