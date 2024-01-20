f1fca66b18266e120e12d7db119ce90287d52db6
Eli-N-Or 2 <eli-n-or@outlook.com>
2024-01-20 17:07:41 +0200

    Added no input message to blank edited links

--- routes/edit.router.ts
+++ routes/edit.router.ts
@@ -13,22 +13,27 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.patch("/:id", asyncRoute(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    if (req.body.new_link.length < 8){
-        const redirectObj: RedirectObject | boolean | undefined = await edit_controller.editLink(
-            req.params.id, 
-            req.body.new_link
-            );
-        if (redirectObj === false){
-            req.no_path_err = "Path is already in use";
-            noPathHandler(req, res);
+    if (req.body.new_link.length === 0){
+        req.no_path_err = "No input";
+        noPathHandler(req, res);
+    } else {
+        if (req.body.new_link.length < 8){
+            const redirectObj: RedirectObject | boolean | undefined = await edit_controller.editLink(
+                req.params.id, 
+                req.body.new_link
+                );
+            if (redirectObj === false){
+                req.no_path_err = "Path is already in use";
+                noPathHandler(req, res);
+            } else {
+                redirectObj !== undefined && typeof redirectObj !== "boolean" ? 
+                res.status(200).json({_id: redirectObj._id, output: redirectObj.output}): 
+                noPathHandler(req, res);
+            }
         } else {
-            redirectObj !== undefined && typeof redirectObj !== "boolean" ? 
-            res.status(200).json({_id: redirectObj._id, output: redirectObj.output}): 
+            req.no_path_err = "Link is too long";
             noPathHandler(req, res);
         }
-    } else {
-        req.no_path_err = "Link is too long";
-        noPathHandler(req, res);
     }
 }));
 
